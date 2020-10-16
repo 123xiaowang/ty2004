5356d9957c92905cd9b25764bd0abe7d1f70ae4f
123xiaowang <2548729739@qq.com>
2020-10-16 10:33:59 +0800

    8.实现拖拽

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var a = 2;
-var b = 4 ;
-var c= 3 ;
+function drag(selector){
+     let ele = document.querySelector(selector);
+     ele.onmousedown = function (evt){
+         let e = evt || window.event;
+         let distX = e.offsetX;
+         let distY = e.offsetY;
+         document.onmousemove = function(evt){
+            let e = evt || window.event;
+            ele.style.left = e.pageX  - distX + "px";
+            ele.style.top = e.pageY - distY + "px";
+         }
+         document.onmouseup = function(){
+             document.onmousemove = null;
+         }
+         document.ondragstart = function(){
+             return false;
+         }
+     }
+}
